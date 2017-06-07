a48f9896d836f6e7bd40dde354818332f7ad7b1d
Felicio Hoffmann <hoffmann.felicio@gmail.com>
2017-06-07 19:09:40 -0300

    -all

--- src/test/java/Webdriver.java
+++ src/test/java/Webdriver.java
@@ -3,6 +3,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -12,8 +13,9 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class Webdriver {
 	static final String PASSWORD = System.getenv("TRpassword");
 	static final String USERNAME = System.getenv("TRuser");
-	static final String RELEASE = "Release_36.3";
+	static final String[] RELEASE = 	{"Release_36.3", "Release_36.3_-_Homologação"};
 	static final String ISSUES = "//a[contains(@class, 'issue tracker')]";
+	static final String QADOC = "//td[contains(text(),'Q.A. Doc')]/";//*[contains(text(),'match')]
 	
 	public void login(WebDriver driver){
 		driver.findElement(By.className("login")).click();
@@ -28,24 +30,35 @@ public class Webdriver {
 		System.setProperty("webdriver.chrome.driver", "C:/Users/Felicio/Downloads/chromedriver_win32/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		//faz o webdriver esperar pelo menos 3 segundos antes de dar timeout caso nao ache o elemento
-		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-		driver.get("http://redmine.ss.local/projects/evoluservices/roadmap/");
-		driver.findElement(By.name(RELEASE)).click();
-		String release = driver.getCurrentUrl();
-		
-		int i =0;
-		do{
-			List<WebElement> issues = driver.findElements(By.xpath(ISSUES));
-			System.out.println(issues.get(i).getText());
-			issues.get(i).click();
-			
-			
-			
-			driver.navigate().back();
-			i++;
-			if (i == issues.size()) i = -1;
-			
-		}while (i != -1);
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+
+
+
+		for (int j = 0; j < RELEASE.length ; j++ ) {
+			driver.get("http://redmine.ss.local/projects/evoluservices/roadmap/");
+			driver.findElement(By.name(RELEASE[j])).click();
+			int i=0;
+			do {
+
+				List<WebElement> issues = driver.findElements(By.xpath(ISSUES));
+				String type = issues.get(i).getText();
+				System.out.println(type);
+				issues.get(i).click();
+				if(!type.toLowerCase().contains("Bug".toLowerCase())) {
+					try {
+						driver.findElement(By.xpath(QADOC));
+						System.out.println("QAdoc" + i);
+						driver.navigate().back();
+					} catch (NoSuchElementException e) {
+						System.out.println("Issues sem Q.A. Doc");
+					}
+				}
+				driver.navigate().back();
+				i++;
+				if (i == issues.size()) i = -1;
+
+			} while (i != -1);
+		}
 		
 		
 	}
