62ac79c58a43d788c91ebca4a662dc42e714c96f
Baether <hoffmann.felicio@gmail.com>
2017-06-07 23:27:32 -0300

    First commit using intellij

--- src/test/java/Webdriver.java
+++ src/test/java/Webdriver.java
@@ -31,16 +31,16 @@ public class Webdriver {
 
 	@Test
 	public void createTestPlan() {
-		/*System.setProperty("webdriver.chrome.driver", "C:/Users/Felicio/Downloads/chromedriver_win32/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:/Users/Felicio/Downloads/chromedriver_win32/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		//faz o webdriver esperar pelo menos 3 segundos antes de dar timeout caso nao ache o elemento
 		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 
+		System.out.println("voce comitou com sucesso pelo intellij e adicionou o foreach");
 
-
-		for (int j = 0; j < RELEASE.length ; j++ ) {
+		for (String release : RELEASE ) {
 			driver.get("http://redmine.ss.local/projects/evoluservices/roadmap/");
-			driver.findElement(By.name(RELEASE[j])).click();
+			driver.findElement(By.name(release)).click();
 			int i=0;
 			do {
 
@@ -62,8 +62,6 @@ public class Webdriver {
 				if (i == issues.size()) i = -1;
 
 			} while (i != -1);
-		}*/
-
-		
+		}
 	}
 }
